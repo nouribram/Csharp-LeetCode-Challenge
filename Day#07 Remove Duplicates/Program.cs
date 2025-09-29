6d5db3bd63940195953bffb14009a8d34a5e8ffa
nouribram <nouribram3@gmail.com>
2025-09-29 09:58:57 +0300

    new update

--- Day#07 Remove Duplicates/Program.cs
+++ Day#07 Remove Duplicates/Program.cs
@@ -11,39 +11,19 @@
 // - Finally, writeIndex represents the length of the array without duplicates.
 // --------------------------------------------------
 
-using System;
-public class RemoveDuplicatesSolution
-{
-    public static int RemoveDuplicates(int[] nums)
-    {
-        if (nums.Length == 0)
-            return 0;
 
-        int writeIndex = 1; // First element is always unique
-
-        for (int i = 1; i < nums.Length; i++)
-        {
-            if (nums[i] != nums[i - 1]) // Found new unique element
-            {
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if (nums.Length == 0) return 0;
+        
+        int writeIndex = 1;
+        for (int i = 1; i < nums.Length; i++) {
+            if (nums[i] != nums[i - 1]) {
                 nums[writeIndex] = nums[i];
                 writeIndex++;
             }
         }
-
         return writeIndex;
     }
-
-    // Test the solution
-    public static void Main(string[] args)
-    {
-        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-        int length = RemoveDuplicates(nums);
-
-        Console.WriteLine("Unique Length: " + length);
-        Console.Write("Array after removing duplicates: ");
-        for (int i = 0; i < length; i++)
-        {
-            Console.Write(nums[i] + " ");
-        }
-    }
 }
+
